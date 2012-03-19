4bf32cbaeadf4d7c7725009fda18b073e9598eef
Taekmin Kim <tantara.tm@gmail.com>
2012-03-20 01:31:46 +0900

    make new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cnshs::Application.config.secret_token = 'bd56f5b072478ea44808801e6b229495b2b991bcd4ddfc5fecc05fc6f48e688d40ff488f836e57b3b76be32e39f6e09d74cfdcd14aa557408451d624d80a751a'
